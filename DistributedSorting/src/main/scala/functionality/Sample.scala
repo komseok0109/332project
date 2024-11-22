92afdd2fdc1d44a04931dde3eec6e26cf730e6c9
ha1238 <ha1238@postech.ac.kr>
2024-11-22 15:22:58 +0900

    feat: add sampling code

---
+++ DistributedSorting/src/main/scala/functionality/Sample.scala
@@ -0,0 +1,40 @@
+package functionality
+
+import scala.concurrent._
+import ExecutionContext.Implicits.global
+import scala.io.Source
+import scala.util.Random
+import scala.concurrent.duration._
+import java.util.concurrent.ConcurrentLinkedQueue
+import scala.jdk.CollectionConverters._
+import com.typesafe.scalalogging.LazyLogging
+
+object Sample extends LazyLogging {
+  private val NUM_MAX_SAMPLE = 100000
+
+  def sampleFile(filesPathList: List[String]): List[String] = {
+    val sampleData = new ConcurrentLinkedQueue[String]()
+
+    val numGroups = math.max(1, filesPathList.length / 4)
+    val sampleNumOfEachFile = NUM_MAX_SAMPLE / filesPathList.length
+    val futures = filesPathList.grouped(numGroups).map { group =>
+      Future {
+        group.foreach { filePath =>
+          try {
+            val source = Source.fromFile(filePath)
+            val linesList = source.getLines().map(_.splitAt(10)).toList
+            source.close()
+            val keyArray = linesList.map(_._1).toArray
+            for (_ <- 0 until sampleNumOfEachFile) {
+              sampleData.add(keyArray(Random.nextInt(keyArray.length)))
+            }
+          } catch {
+            case ex: Exception => logger.error(s"Error processing file $filePath: ${ex.getMessage}")
+          }
+        }
+      }
+    }
+    Await.result(Future.sequence(futures.toList), Duration.Inf)
+    sampleData.asScala.toList
+  }
+}
