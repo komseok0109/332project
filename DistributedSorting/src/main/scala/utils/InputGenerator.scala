b283500e5383a9b51b226206abc026cf5fb7ec9f
shinjw4929 <sjw4929@gmail.com>
2024-11-22 15:33:24 +0900

    scala : complete InputGenerator

---
+++ DistributedSorting/src/main/scala/utils/InputGenerator.scala
@@ -0,0 +1,74 @@
+package utils
+
+import utils.IOUtils.getFilePathsFromDirectories
+import scala.io.Source
+import java.io.{File, PrintWriter}
+import scala.concurrent._
+import java.util.concurrent.Executors
+import scala.concurrent.duration._
+
+object InputGenerator {
+  def main(args: Array[String]): Unit = {
+    val directories = List("DistributedSorting/input1", "DistributedSorting/input2", "DistributedSorting/input3")
+    val filePaths = getFilePathsFromDirectories(directories)
+    getInput(filePaths)
+  }
+
+  def getInput(filePathList: List[String]): Unit = {
+    // 출력 디렉토리 생성
+    val outputDir = new File("DistributedSorting/output")
+    if (!outputDir.exists()) outputDir.mkdirs()
+
+    // 고정 크기의 스레드 풀 생성
+    val availableProcessors = Runtime.getRuntime.availableProcessors()
+    implicit val ec = ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(availableProcessors))
+
+    try {
+      // 파일별 Iterator 처리
+      filePathList.zipWithIndex.foreach { case (oneFile, fileIndex) =>
+        val source = Source.fromFile(oneFile)
+        try {
+          // 파일에서 320000줄씩 청크 Iterator 생성
+          val lineChunks = getLineChunks(source.getLines(), 320000)
+
+          // 청크 처리
+          lineChunks.zipWithIndex.foreach { case (chunk, chunkIndex) =>
+            val chunkFilePath = s"DistributedSorting/output/file${fileIndex}_chunk$chunkIndex.txt"
+
+            // 디렉토리 생성 확인
+            val chunkFile = new File(chunkFilePath)
+            if (!chunkFile.getParentFile.exists()) chunkFile.getParentFile.mkdirs()
+
+            // 청크 데이터를 비동기적으로 저장
+            val future = Future {
+              val writer = new PrintWriter(chunkFile)
+              try {
+                chunk.foreach(writer.println)
+              } finally {
+                writer.close()
+              }
+              println(s"File $fileIndex, Chunk $chunkIndex 저장 완료: $chunkFilePath")
+            }
+
+            // 처리 완료 대기
+            Await.result(future, Duration.Inf)
+          }
+        } finally {
+          // 파일 소스 닫기
+          source.close()
+        }
+      }
+    } finally {
+      // 스레드 풀 종료
+      ec.shutdown()
+    }
+  }
+
+  // 파일을 지정한 크기의 청크로 Iterator를 반환하는 함수
+  def getLineChunks(linesIterator: Iterator[String], chunkSize: Int): Iterator[List[String]] = {
+    new Iterator[List[String]] {
+      def hasNext: Boolean = linesIterator.hasNext
+      def next(): List[String] = linesIterator.take(chunkSize).toList
+    }
+  }
+}
