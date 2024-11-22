9f60ae52569ef06b71c6e1c6320276d8f9d2088e
komseok0109 <komseok0109@gmail.com>
2024-11-22 14:35:27 +0900

    feat: add interface related to I/O

---
+++ DistributedSorting/src/main/scala/utils/IOUtils.scala
@@ -0,0 +1,24 @@
+package utils
+
+import java.nio.file.{Files, Path, Paths}
+import scala.jdk.CollectionConverters._
+import com.typesafe.scalalogging.LazyLogging
+
+object IOUtils extends LazyLogging {
+  def getFilePathsFromDirectories(directories: List[String]): List[String] = {
+    directories.flatMap { dir =>
+      val path = Paths.get(dir)
+      if (Files.exists(path) && Files.isDirectory(path)) {
+        Files.walk(path)
+          .iterator()
+          .asScala
+          .filter(Files.isRegularFile(_))
+          .map(_.toString)
+          .toList
+      } else {
+        logger.warn(s"Warning: $dir is not a valid directory or does not exist.")
+        throw new IllegalArgumentException(s"Invalid directory: $dir")
+      }
+    }
+  }
+}
