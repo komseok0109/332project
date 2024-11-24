650019f3c5cfdb5cac53628f3f19714cecac04da
komseok0109 <komseok0109@gmail.com>
2024-11-24 19:32:03 +0900

    test: edit input data generation code

--- DistributedSorting/src/main/scala/utils/InputGenerator.scala
+++ DistributedSorting/src/main/scala/utils/InputGenerator.scala
@@ -10,34 +10,15 @@ import scala.concurrent.duration._
 
 object InputGenerator {
   def main(args: Array[String]): Unit = {
-    val inputGenerator = new InputGenerator(args.toList)
-    inputGenerator.generateInputs()
+    val filePaths = getFilePathsFromDirectories(args.toList)
+    generateInput(filePaths)
+    deleteFiles(filePaths)
   }
-}
-
-class InputGenerator(inputDirectories: List[String]) {
   private val CHUNK_SIZE = 320000
-
-  def generateInputs(): Unit = {
-    inputDirectories.foreach(dir => {
-      val filePaths = getFilePathsFromDirectories(List(dir))
-      generateInput(dir, filePaths)
-      deleteFiles(filePaths)
-    })
-  }
-
-<<<<<<< HEAD
-  private def generateInput(directory: String, filePathList: List[String]): Unit = {
-    val outputDir = new File(directory)
-    if (!outputDir.exists()) outputDir.mkdirs()
-
-=======
-  def getInput(filePathList: List[String]): Unit = {
-    // 고정 크기의 스레드 풀 생성
->>>>>>> origin/shin
+  private def generateInput(filePathList: List[String]): Unit = {
     val availableProcessors = Runtime.getRuntime.availableProcessors()
-    implicit val ec: ExecutionContextExecutorService =
-      ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(availableProcessors))
+    implicit val ec: ExecutionContextExecutorService
+      = ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(availableProcessors))
 
     try {
       filePathList.zipWithIndex.foreach { case (oneFile, fileIndex) =>
@@ -46,13 +27,8 @@ class InputGenerator(inputDirectories: List[String]) {
           val lineChunks = getLineChunks(source.getLines(), CHUNK_SIZE)
 
           lineChunks.zipWithIndex.foreach { case (chunk, chunkIndex) =>
-<<<<<<< HEAD
-            val chunkFilePath = directory + "/file${fileIndex}_chunk$chunkIndex"
-=======
-            // 원래 파일이 있는 디렉토리 기반으로 출력 파일 경로 생성
             val originalDir = new File(oneFile).getParent
             val chunkFilePath = s"$originalDir/file${fileIndex}_$chunkIndex.txt"
->>>>>>> origin/shin
 
             val chunkFile = new File(chunkFilePath)
             if (!chunkFile.getParentFile.exists()) chunkFile.getParentFile.mkdirs()
@@ -64,6 +40,7 @@ class InputGenerator(inputDirectories: List[String]) {
               } finally {
                 writer.close()
               }
+              println(s"File $fileIndex, Chunk $chunkIndex 저장 완료: $chunkFilePath")
             }
 
             Await.result(future, Duration.Inf)
