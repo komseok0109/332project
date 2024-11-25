22bf1e9296b79248fbb319dbd340716cb886848a
shinjw4929 <sjw4929@gmail.com>
2024-11-25 22:55:35 +0900

    feat: edit partitioning

--- DistributedSorting/src/main/scala/functionality/SortAndPartition.scala
+++ DistributedSorting/src/main/scala/functionality/SortAndPartition.scala
@@ -1,12 +1,13 @@
 package functionality
 
 import scala.io.Source
-import java.io.{File, PrintWriter, BufferedWriter, FileWriter}
+import java.io.{BufferedWriter, File, FileWriter, PrintWriter}
 import scala.collection.mutable
 import scala.concurrent._
 import java.util.concurrent.Executors
 import scala.concurrent.duration._
 import com.typesafe.scalalogging.LazyLogging
+import utils.IOUtils.getFilePathsFromDirectories
 
 object SortAndPartition extends LazyLogging {
   def openFileAndProcessing(filePaths: List[String], key2Ranges: List[(Int, (String, String))],
@@ -15,77 +16,52 @@ object SortAndPartition extends LazyLogging {
     val executorService = Executors.newFixedThreadPool(numThreads)
     implicit val ec: ExecutionContext = ExecutionContext.fromExecutorService(executorService)
     try {
-      val processingFutures = processChunks(filePaths, key2Ranges, outputDir, currentWorkerID)
+      val processingFutures = filePaths.map { filePath =>
+        Future {
+          processFile(filePath, key2Ranges, outputDir, currentWorkerID)
+        }
+      }
       Await.result(Future.sequence(processingFutures), Duration.Inf)
     } finally {
       executorService.shutdown()
     }
   }
 
-  private def processChunks(filePaths: List[String], keyRanges: List[(Int, (String, String))], outputDir: String,
-                            currentWorkerID: Int)(implicit ec: ExecutionContext): Seq[Future[Unit]] = {
-    val chunkSizeInBytes = 10 * 1024 * 1024
-    val buffer = mutable.ArrayBuffer[String]()
-    var currentChunkSize = 0L
-    var chunkIndex = 0
-
-    val fileIterator = filePaths.iterator
-    var currentSource: Option[Source] = None
-    var lineIterator: Iterator[String] = Iterator.empty
-
-    val processingFutures = mutable.Buffer[Future[Unit]]()
-
+  def processFile(filePath: String, key2Ranges: List[(Int, (String, String))], outputDir: String, currentWorkerID: Int): Unit = {
+    val chunkSize = 1000000
     try {
-      while (fileIterator.hasNext || lineIterator.hasNext) {
-        if (!lineIterator.hasNext && fileIterator.hasNext) {
-          val filePath = fileIterator.next()
-          try {
-            currentSource.foreach(_.close())
-            currentSource = Some(Source.fromFile(filePath))
-            lineIterator = currentSource.get.getLines()
-          } catch {
-            case e: Exception =>
-              logger.error(s"Error during file($filePath) read: ${e.getMessage}")
-              lineIterator = Iterator.empty
-          }
-        }
-
-        while (currentChunkSize < chunkSizeInBytes && lineIterator.hasNext) {
-          val line = lineIterator.next()
-          buffer += line
-          currentChunkSize += line.getBytes("UTF-8").length + 1
-        }
-
-        if (currentChunkSize >= chunkSizeInBytes ||
-          (!fileIterator.hasNext && !lineIterator.hasNext && buffer.nonEmpty)) {
-          val chunkData = buffer.toArray
-          val currentChunkIndex = chunkIndex
-          val future = Future {
-            processChunk(chunkData, keyRanges, currentChunkIndex, outputDir, currentWorkerID)
-          }
-          processingFutures += future
-          chunkIndex += 1
+      val source = Source.fromFile(filePath)
+      try {
+        val lineIterator = source.getLines()
+        val buffer = mutable.ArrayBuffer[String]()
+        var chunkIndex = 0
+        while (lineIterator.hasNext) {
           buffer.clear()
-          currentChunkSize = 0L
+          while (buffer.size < chunkSize && lineIterator.hasNext) {
+            buffer += lineIterator.next()
+          }
+          if (buffer.nonEmpty) {
+            processChunk(buffer.toArray, key2Ranges, filePath, chunkIndex, currentWorkerID)
+            chunkIndex += 1
+          }
         }
+      } finally {
+        source.close()
       }
-    } finally {
-      currentSource.foreach(_.close())
+    } catch {
+      case e: Exception =>
+        logger.error(s"Error during file($filePath) read: ${e.getMessage}")
     }
-
-    processingFutures.toSeq
   }
-
-  private def processChunk(chunk: Array[String], keyRanges: List[(Int, (String, String))],
-                           chunkIndex: Int, outputDir: String, currentWorkerID: Int): Unit = {
+  def processChunk(chunk: Array[String], key2Ranges: List[(Int, (String, String))], filePath: String, chunkIndex: Int, currentWorkerID: Int): Unit = {
     val linesByRange = new mutable.HashMap[Int, mutable.ArrayBuffer[String]]()
 
     chunk.foreach { line =>
       if (line.nonEmpty) {
-        val rangeInfo = findRange(line, keyRanges)
-        rangeInfo match {
-          case Some((workerId, rangeLabel)) =>
-            val lines = linesByRange.getOrElseUpdate(workerId, mutable.ArrayBuffer[String]())
+        val rangeKey = findRange(line, key2Ranges)
+        rangeKey match {
+          case Some(key) =>
+            val lines = linesByRange.getOrElseUpdate(key._1, mutable.ArrayBuffer[String]())
             lines += line
           case None =>
             logger.warn(s"Line is not assigned to any range: $line")
@@ -93,9 +69,10 @@ object SortAndPartition extends LazyLogging {
       }
     }
 
-    linesByRange.foreach { case (label, lines) =>
+    linesByRange.foreach { case (key, lines) =>
       val sortedLines = lines.sorted
-      val outputPath = s"$outputDir/$label/chunk_$chunkIndex _worker_$currentWorkerID.txt"
+      val sanitizedFilePath = filePath.replaceAll("[^a-zA-Z0-9.-]", "_")
+      val outputPath = s"output/$key/${sanitizedFilePath}_chunk_$chunkIndex _Worker $currentWorkerID.txt"
       val outputFile = new File(outputPath)
 
       val parentDir = outputFile.getParentFile
@@ -119,6 +96,7 @@ object SortAndPartition extends LazyLogging {
       }
     }
   }
+
 
   private def findRange(line: String, keyRanges: List[(Int, (String, String))]): Option[(Int, String)] = {
     var low = 0
