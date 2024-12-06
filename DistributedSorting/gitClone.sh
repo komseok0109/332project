12664471729005ff56c667e2bdecf42a11750a5f
komseok0109 <komseok0109@gmail.com>
2024-12-06 14:49:42 +0900

    feat: add shell scripts

---
+++ DistributedSorting/gitClone.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+WORKERS_FILE="/home/white/deployment/workers.txt"
+
+# workers.txt 파일에서 IP 주소를 읽어와서 루프
+while IFS= read -r worker_ip; do
+    echo "Deploying to $worker_ip"
+
+    ssh -n white@$worker_ip "
+        git clone --branch main https://github.com/komseok0109/332project.git
+    " || echo "Failed to execute git pull on $worker_ip"
+done < "$WORKERS_FILE"
+
+echo "Deployment complete"
